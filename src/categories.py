0257434c4de727c27949f9e34086881923e37d40
migue456456 <migue456456@mail.fresnostate.edu>
2025-12-09 21:25:41 -0800

    Add categories for various terms in categories.py

---
+++ src/categories.py
@@ -0,0 +1,15 @@
+beat = {"beat", "fight"}
+weapon = {"gat", "gats", "gun", "guns", "shot", "shots", "gunshot", "gunshots"}
+lawenforcer = {"police", "cop", "fbi"}
+fox = {"fox", "foxs"}
+murder = {"murder", "murders", "murderers", "murdered", "die", "dies", "kill", "killing"}
+color = {"black", "brown", "color"}
+
+categories = {
+    "beat": beat,
+    "weapon": weapon,
+    "lawenforcer": lawenforcer,
+    "fox": fox,
+    "murder": murder,
+    "color": color
+}
