c8aa8cc9d8cbfbb9203b3faff7c6e4b9dd7d3e72
migue456456 <migue456456@mail.fresnostate.edu>
2025-12-18 12:51:57 -0800

    Add Cleaner class for text cleaning utilities
    Introduced a Cleaner class to encapsulate text cleaning utilities and store word categories as an instance attribute.

--- src/categories.py
+++ src/categories.py
@@ -1,15 +1,27 @@
-beat = {"beat", "fight"}
-weapon = {"gat", "gats", "gun", "guns", "shot", "shots", "gunshot", "gunshots"}
-lawenforcer = {"police", "cop", "fbi"}
-fox = {"fox", "foxs"}
-murder = {"murder", "murders", "murderers", "murdered", "die", "dies", "kill", "killing"}
-color = {"black", "brown", "color"}
+class Cleaner:
+    '''
+    Represents text cleaning utilities
+    Attributes:
+        categories: dictionary of word categories for analysis
+    '''
 
-categories = {
-    "beat": beat,
-    "weapon": weapon,
-    "lawenforcer": lawenforcer,
-    "fox": fox,
-    "murder": murder,
-    "color": color
-}
+    def __init__(self):
+        # define categories as attribute so other methods can use them
+        self.categories = {
+            "beat": {"beat", "fight"},
+            "weapon": {"pistol","pistols","ak","gat", "gats", "gun", "guns", "shot", "shots", "gunshot", "gunshots"},
+            "lawenforcer": {"police", "cop", "fbi"},
+            "fox": {"fox", "foxs"},
+            "murder": {"murder", "murders", "murderers", "murdered", "die", "dies", "kill", "killing"},
+            "color": {"black", "brown", "color"}
+        }
+
+    def get_categories(self):
+        '''
+        Returns the categories dictionary
+        '''
+        return self.categories
+
+    # You could add more cleaning functions here later, e.g., text cleaning
+    # def clean_text(self, text):
+    #     """Example: remove punctuation, lowercase, replace words, etc."""
